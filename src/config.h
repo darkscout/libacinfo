60a324ef959de830413994316e571d61e3ce90ec
Swen Weiland <Swen.Weiland@gmx.net>
2010-10-13 12:38:25 +0200

    BUGFIX: again convert2flow
    NOTE: "Kalibrierwert in Impulse/Liter" is not stored in the pump.
    Needs to be adjusted in src/config.h if you have a water flow sensor.

---
+++ src/config.h
@@ -0,0 +1,22 @@
+#ifndef CONFIG_H
+#define CONFIG_H 1
+
+
+// see sensor manual or forum for values
+
+//#define SENSOR_FLOW_IMPULSE_PER_LITER 256 // aquasuite default
+#define AQUASTREAMXT_SENSOR_FLOW_IMPULSE_PER_LITER 169
+
+
+
+/*
+AQUASTREAMXT DEFAULT VALUES:
+	Measure Edges FAN = 8
+	Measure Edges FLOW = 8
+	
+	Impulses per Liter = 256 (0x100)
+	
+*/
+
+
+#endif
